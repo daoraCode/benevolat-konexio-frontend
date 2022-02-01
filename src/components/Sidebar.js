2a7f08b1f78085e5efa2eb05d930bd89add39304
daoraCode <severin.mboukou@gmail.com>
2022-02-01 17:13:44 +0100

    initial commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from '../images/konexio.png';
+
+import { Link } from "react-router-dom";
+
+const Nav = styled.nav`
+    padding: 0;
+    margin: 0;
+    background-color: #0375BB;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 390px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+`
+
+const Img = styled.img`
+    padding-inline: 88px; 
+    width: 100%;
+    width: 213px;  
+    align-self: center;
+    align-content: center;
+    justify-content: center;    
+    background-color: #fff;
+`
+
+const H1 = styled.h1`
+    font-family: Poppins;
+    text-align: center;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 205px;
+    text-decoration: none;
+    color: #fff;
+`
+
+const Sidebar = () => {
+    return (
+        <Nav>
+            <Img src={Logo} />
+            <Link to='/home'>
+                <H1>Home</H1>
+            </Link>
+            <Link to='/'>
+                <H1>Liste de bénévoles</H1>
+            </Link>
+            <Link to='/'>
+                <H1>News</H1>
+            </Link>
+            <Link to='/'>
+                <H1>F.A.Q</H1>
+            </Link>
+            <Link to='/'>
+                <H1>Messagerie</H1>
+            </Link>
+        </Nav>
+    );
+};
+
+export default Sidebar;
