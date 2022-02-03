f9b09c4c40acdba62f3fca8c8afb64325507d802
KevinJeanMarie <kjeanmarie01@gmail.com>
2022-02-03 21:21:26 +0100

    page news ok

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,30 +10,35 @@ const Nav = styled.nav`
     background-color: #0375BB;
     top: 0;
     left: 0;
-    height: 100%;
     width: 390px;
+    height: 100vh;
     position: relative;
     display: flex;
     flex-direction: column;
+
+    @media (min-height: 800px) {
+        height: 100%;
+    }
 `
 
 const Img = styled.img`
-    padding-inline: 88px; 
-    width: 100%;
-    width: 213px;  
+    padding-inline: 100px; 
+    width: 190px;
     align-self: center;
     align-content: center;
     justify-content: center;    
     background-color: #fff;
+    margin-bottom: 20px;
+
 `
 
 const H1 = styled.h1`
-    font-family: Poppins;
+    font-family: 'Poppins';
     text-align: center;
     font-size: 30px;
     font-style: normal;
     font-weight: 400;
-    line-height: 205px;
+    line-height: 80px;
     text-decoration: none;
     color: #fff;
 `
@@ -42,19 +47,19 @@ const Sidebar = () => {
     return (
         <Nav>
             <Img src={Logo} />
-            <Link to='/home'>
+            <Link to='/home' className="link nav">
                 <H1>Home</H1>
             </Link>
-            <Link to='/'>
+            <Link to='/' className="link nav">
                 <H1>Liste de bénévoles</H1>
             </Link>
-            <Link to='/news'>
+            <Link to='/news' className="link nav">
                 <H1>News</H1>
             </Link>
-            <Link to='/'>
+            <Link to='/' className="link nav">
                 <H1>F.A.Q</H1>
             </Link>
-            <Link to='/messagerie'>
+            <Link to='/messagerie' className="link nav">
                 <H1>Messagerie</H1>
             </Link>
         </Nav>
--- src/pages/News.js
+++ src/pages/News.js
@@ -3,27 +3,30 @@ import Sidebar from '../components/Sidebar';
 import styled from 'styled-components';
 
 const Newnews = styled.div`
-.news {
-    display: flex;
-}
+display: flex;
+// .news {
+//     display: flex;
+//     flex-direction: row;
+// }
 
 .news h2 {
-    font-size: 50px;
+    font-size: 60px;
     margin-left: 50px;
     color: #0375BB;
     display: flex;
+    margin-top: 20px;
 }
 
-.contents p {
-        background-color: #BEBEBE;
+.news p {
+        background-color: #D7D8D8;
         border-radius: 10px;
         font-size: 15px;
         padding-left: 30px;
-        margin-right: 100px;
-        text-align: center;
-        margin-top: 150px;
+        margin-right: 130px;
+        margin-top: 25px;
         padding-top: 20px;
         padding-bottom: 20px;
+        margin-left: 50px;
 
 }
 
@@ -35,17 +38,39 @@ const Newnews = styled.div`
 const News = () => {
     return (
         <Newnews>
+            <Sidebar />  
         <div className="news">
-            <Sidebar />   
             <h2>News</h2>
-            <div className="contents">
+            <p> Bonjour, 
+                    veuillez notez que la session Digistart du 29 mai 2022
+                    sera décalée d'une semaine, pour une question d'organisation.
+                    <br/>
+                    Merci.                    
+                </p>
+
                 <p> Bonjour, 
                     veuillez notez que la session Digistart du 29 mai 2022
                     sera décalée d'une semaine, pour une question d'organisation.
                     <br/>
                     Merci.                    
-                </p> 
-        </div>
+                </p>
+
+                <p> Bonjour, 
+                    veuillez notez que la session Digistart du 29 mai 2022
+                    sera décalée d'une semaine, pour une question d'organisation.
+                    <br/>
+                    Merci.                    
+                </p>
+
+                <p> Bonjour, 
+                    veuillez notez que la session Digistart du 29 mai 2022
+                    sera décalée d'une semaine, pour une question d'organisation.
+                    <br/>
+                    Merci.                    
+                </p>
+         
+            </div>
+            <div className="contents"> 
         </div>
         </Newnews>
     );
