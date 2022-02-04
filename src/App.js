793f202238855cd283dbbd39bf0f05dc4983f3cc
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2022-02-04 11:36:39 +0100

    home done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Error404 from "./pages/Error404";
-import Home from "./pages/Home";
+import Sessions from "./pages/Sessions";
 import { ListSessionsContextProvider } from "./context/ListSessions";
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
     <BrowserRouter>
       <ListSessionsContextProvider>
         <Routes>
-          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/sessions" element={<Home />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
       </ListSessionsContextProvider>
